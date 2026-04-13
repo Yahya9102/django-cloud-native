6547b3cb0c16f712e3afb2b710625c31d9798828
Yahya9102 <yaya_hussein@hotmail.com>
2026-04-13 16:29:08 +0200

    add support for category in notes

--- notes/models.py
+++ notes/models.py
@@ -1,9 +1,19 @@
 from django.db import models
 
+from categories.models import Category
+
+
 # Create your models here.
 class Note(models.Model):
     title = models.CharField(max_length=100)
     content = models.TextField()
+    category = models.ForeignKey(
+        Category,
+        on_delete=models.CASCADE,
+        related_name="notes",
+        null=True,
+        blank=True
+    )
 
     def __str__(self):
         return self.title
--- notes/views.py
+++ notes/views.py
@@ -4,6 +4,7 @@ from django.http import JsonResponse
 from django.views.decorators.csrf import csrf_exempt
 
 from .models import Note
+from categories.models import Category
 
 
 # Create your views here.
@@ -23,8 +24,7 @@ from .models import Note
 @csrf_exempt
 def create_note(request):
     if request.method == "GET":
-        notes = Note.objects.all()
-
+        notes = Note.objects.select_related("category").all()
 
         data = []
 
@@ -33,22 +33,48 @@ def create_note(request):
                 "id": note.id,
                 "title": note.title,
                 "content": note.content,
+                "category": {
+                    "id": note.category.id,
+                    "name": note.category.name
+                }
             })
-
             return JsonResponse(data, safe=False)
 
     elif request.method == "POST":
         body = json.loads(request.body)
 
+        category_id = body.get("category_id")
+
+        if not category_id:
+            return JsonResponse(
+                {"error": "category id is required"},
+                status=400
+            )
+        
+        try:
+            category = Category.objects.get(id=category_id)
+        except Category.DoesNotExist:
+            return JsonResponse(
+                {"error": "category not found"},
+                status=404
+            )    
+
+
+
         note = Note.objects.create(
             title=body.get("title"),
             content=body.get("content"),
+            category=category
         )
 
         return JsonResponse({
             "id": note.id,
             "title": note.title,
             "content": note.content,
+            "category": {
+                    "id": note.category.id,
+                    "name": note.category.name
+                }
         }, status=201)
     
     return JsonResponse({"error": "Method not allowed"}, status=405)
