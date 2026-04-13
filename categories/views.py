242c04915f270708f41cfffc3081041663a027be
Yahya9102 <yaya_hussein@hotmail.com>
2026-04-13 16:36:42 +0200

    add endpoints in view to category

--- categories/views.py
+++ categories/views.py
@@ -1,3 +1,49 @@
 from django.shortcuts import render
+import json
+
+from django.http import JsonResponse
+from django.views.decorators.csrf import csrf_exempt
+
+from .models import Category
 
 # Create your views here.
+
+@csrf_exempt
+def create_category(request):
+    if request.method == "GET":
+        
+        categories = Category.objects.all()
+
+        data = []
+
+        for category in categories:
+            data.append({
+                "id": category.id,
+                "name": category.name
+                
+            })
+            return JsonResponse(data, safe=False)
+        
+
+    elif request.method == "POST":
+        body = json.loads(request.body)
+
+        name = body.get("name")
+
+        if not name:
+            return JsonResponse(
+                {"error": "category name is required"},
+                status=400
+            )
+        
+        category = Category.objects.create(name=name)
+        
+        return JsonResponse({
+                "id": category.id,
+                 "name": category.name
+                 },
+                status=201
+            )    
+
+    return JsonResponse({"error": "Method not allowed"}, status=405)
+
